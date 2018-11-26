5cbffd13821c4a7372710672538a088888cadef6
micah-jaffe <micah.jaffe95@gmail.com>
2018-11-26 15:43:05 -0800

    Complete currying exercises

--- arguments_exercise.js
+++ arguments_exercise.js
@@ -7,9 +7,9 @@ function sum2(...args) {
   return args.reduce((acc, el) => acc += el);
 }
 
-Function.prototype.myBind = function(ctx, ...args1) {
+Function.prototype.myBind = function(ctxObj, ...args1) {
   return (...args2) => {
-    this.apply(ctx, args1.concat(args2));
+    this.apply(ctxObj, args1.concat(args2));
   };
 };
 
@@ -28,6 +28,46 @@ function curriedSum(numArgs) {
   }
 }
 
+Function.prototype.curry = function(numArgs) {
+  const args = [];
+  const that = this;
+  
+  function _curry(arg) {
+    args.push(arg);
+    if (args.length === numArgs) {
+      return that.apply(null, args);
+    } else {
+      return _curry;
+    }
+  }
+  
+  return _curry;
+};
 
-const newSum = curriedSum(4);
-console.log(newSum(5)(30)(20)(1)); // => 56
+Function.prototype.currySpread = function(numArgs) {
+  const allArgs = [];
+  const that = this;
+  return _curry;
+  
+  function _curry(...args) {
+    allArgs.push(...args);
+    if (allArgs.length === numArgs) {
+      return that.apply(null, allArgs);
+    } else {
+      return _curry;
+    }
+  }
+  
+};
+
+function sumThree(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+function multiplyThree(num1, num2, num3) {
+  return num1 * num2 * num3;
+}
+
+console.log(sumThree.curry(3)(1)(2)(3));
+console.log(multiplyThree.currySpread(3)(1, 3)(3));
+
